5bd8d0998fef3cd5e1d708ff2d962594372fadaf
ROOM3_17 <ROOM3_17@ROOM3_17-PC>
2017-03-23 17:46:53 +0900

    "test2"

--- MoneyBook/src/test/Test.java
+++ MoneyBook/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello, world!");
 		System.out.println("test");
-		// test!!
+		// http://lyb1495.tistory.com/73
 	}
 
 }
